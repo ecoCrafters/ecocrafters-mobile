47258d0b47fac2286c73e438bcaf603ff62442a3
GTHGHT <gilang.example@gmail.com>
2023-05-19 14:45:43 +0800

    Commit App

---
+++ app/src/main/java/com/example/ecocrafters/CreatePostActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ecocrafters
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreatePostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_post)
+    }
+}
